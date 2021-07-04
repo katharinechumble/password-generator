5665f22c11288e654ae090199c81734c2b809130
Katharine C Humble <katharinechumble@gmail.com>
2021-07-04 15:08:06 -0400

    started adding arguments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Assignment code here
 alert("Select from the following options to create a randomly generated secure password.");
-var passwrdLength = prompt("How long do you want your password to be? Passwords may be no shorter than 8 characters and no longer than 128 characters");
-if (passwrdLength <= 7 || passwrdLength >= 129) {
+var passwordLength = prompt("How long do you want your password to be? Passwords may be no shorter than 8 characters and no longer than 128 characters");
+if (passwordLength <= 7 || passwordLength >= 129) {
   alert("Sorry! Passwords may not be longer than 128 characters or shorter than 8 characters.  Please try again!");
 } else {
   var alphaChar = confirm("Would you like to include letters?");
@@ -12,13 +12,22 @@ if (passwrdLength <= 7 || passwrdLength >= 129) {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword(len, charset) {
-  var password = generatePassword();
+function writePassword() {
+  var password = generatePassword(i);
   var passwordText = document.querySelector("#password");
-  const alphaChar = 'abcdefghijklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ';
-  const numChar = '0123456789';
-  const specChar = '!@$%#&*+=?';
-  
+  var letters = 'abcdefghijklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '0123456789';
+  var specialCharacters = '!@$%#&*+=?';
+  var minLength = 8;
+  var maxlength = 128;
+  var newPassword = "";
+  var alphaNumber = letters.concat(numbers);
+  var alphaNumberSpec = letters.concat(numbers,specialCharacters);
+  console.log(writePassword);
+  if (passwordLength >= 8 && passwordLength <= 128 && alphaChar === true) {
+    for (var i = 0; i <= maxlength; i >= minLength, ++i)
+    newPassword = newPassword + letters.charAt(Math.floor(Math.random() * i));
+  }
 
 
   passwordText.value = password;
