5c7ab94ee353e534b4e99e56054a237f67925db1
Katharine C Humble <katharinechumble@gmail.com>
2021-07-04 13:19:53 -0400

    synced style and script sheets and added prompt scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
 // Assignment code here
-
-
+alert("Select from the following options to create a randomly generated secure password.");
+var passwrdLength = prompt("How long do you want your password to be? Passwords may be no shorter than 8 characters and no longer than 128 characters");
+if (passwrdLength <= 7 || passwrdLength >= 129) {
+  alert("Sorry! Passwords may not be longer than 128 characters or shorter than 8 characters.  Please try again!");
+} else {
+  var alphaChar = confirm("Would you like to include letters?");
+  var numChar = confirm("Would you like to include numbers?");
+  var specChar = confirm("Would you like to include special characters?");
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(len, charset) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var 
+  const alphaChar = 'abcdefghijklmnopqrstuvwxyzABCDEGHIJKLMNOPQRSTUVWXYZ';
+  const numChar = '0123456789';
+  const specChar = '!@$%#&*+=?';
+  
 
 
   passwordText.value = password;
